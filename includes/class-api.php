2cd6ef23d15a1d273f1d64447f9c511f88c7499a
Marcus <marcus@marcusbattle.com>
2017-04-24 21:18:47 +0000

    Updated insert_new_post to properly utilize the  for handling non-synced duplicate content

--- includes/class-api.php
+++ includes/class-api.php
@@ -114,62 +114,67 @@ class Press_Sync_API {
 
 	public function insert_new_post( $request ) {
 
-		$post_args 	= $request->get_params();
-		$comments 	= $request->get_param('comments');
+		$post_args 			= $request->get_params();
+		$duplicate_action 	= $request->get_param('duplicate_action');
 
 		if ( ! $post_args ) {
 			return wp_send_json_error();
 		}
 
-		if ( $post = $this->post_exists( $post_args ) ) {
+		// Set the correct post author
+		$post_args['post_author'] = $this->get_press_sync_author_id( $post_args['post_author'] );
 
-			// Attach featured image
-			$this->attach_featured_image( $post['ID'], $post_args );
-
-			// Attach any comments
-			$this->attach_comments( $post['ID'], $comments );
-
-			// Insert Comments
-			// $this->insert_comments( $post['ID'], $post_args );
-
-			// Run any secondary commands
-			do_action( 'press_sync_insert_new_post', $post_id, $post_args );
-
-			// Check if the post has been modified
-			if ( strtotime( $post_args['post_modified'] ) > strtotime( $post['post_modified'] ) ) {
-
-				$post_args['ID'] = $post['ID'];
-
-			} else {
-
-				$data['id'] = $post['ID'];
-				$data['message'] = 'post already exists';
-
-				return wp_send_json_error( $data );
-
-			}
-
-		}
-
-		// Check for post parent and update
+		// Check for post parent and update IDs accordingly
 		if ( isset( $post_args['post_parent'] ) && $post_parent_id = $post_args['post_parent'] ) {
 
 			$post_parent_args['post_type'] = $post_args['post_type'];
 			$post_parent_args['meta_input']['press_sync_post_id'] = $post_parent_id;
 
-			$parent_post = $this->post_exists( $post_parent_args );
+			$parent_post = $this->get_synced_post( $post_parent_args );
 
 			$post_args['post_parent'] = ( $parent_post ) ? $parent_post['ID'] : 0;
 
 		}
 
-		$post_args['post_author'] = $this->get_press_sync_author_id( $post_args['post_author'] );
+		// Check to see if the post exists
+		$local_post = $this->get_synced_post( $post_args );
 
-		$post_id = wp_insert_post( $post_args );
-
-		if ( is_wp_error( $post_id ) ) {
-			return wp_send_json_error( array( 'debug' => $post_id ) );
+		// Check to see a non-synced duplicate of the post exists
+		if ( 'sync' == $duplicate_action && ! $local_post ) {
+			$local_post = $this->get_non_synced_duplicate( $post_args['post_name'], $post_args['post_type'] );
 		}
+
+		// Update the existing ID of the post if present
+		$post_args['ID'] = isset( $local_post['ID'] ) ? $local_post['ID'] : 0;
+
+		// Determine which content is newer, local or remote
+		if ( $local_post && ( strtotime( $local_post['post_modified'] ) >= strtotime( $post_args['post_modified'] ) ) ) {
+
+			// If we're here, then we need to keep our local version
+			$data['remote_post_id']	= $post_args['meta_input']['press_sync_post_id'];
+			$data['local_post_id'] 	= $local_post['ID'];
+			$data['message'] 		= __( 'Local version is newer than remote version', 'press-sync' );
+
+			// Assign a press sync ID
+			$this->add_press_sync_id( $local_post['ID'], $post_args );
+
+			return wp_send_json_error( array( 'debug' => $data ) );
+
+		}
+
+		// Insert/update the post
+		$local_post_id = wp_insert_post( $post_args );
+
+		// Bail if the insert didn't work
+		if ( is_wp_error( $post_id ) ) {
+			return wp_send_json_error( array( 'debug' => $local_post_id ) );
+		}
+
+		// Attach featured image
+		$this->attach_featured_image( $local_post_id, $post_args );
+
+		// Attach any comments
+		$this->attach_comments( $local_post_id, $request->get_param('comments') );
 
 		// Set taxonomies for custom post type
 		if ( ! in_array( $post_args['post_type'], array( 'post', 'page' ) ) ) {
@@ -184,21 +189,14 @@ class Press_Sync_API {
 
 		}
 
-		// Attach featured image
-		$this->attach_featured_image( $post_id, $post_args );
-
-		// Attach any comments
-		$this->attach_comments( $post_id, $comments );
-
-		// Insert Comments
-		// $this->insert_comments( $post_id, $post_args );
-
 		// Run any secondary commands
 		do_action( 'press_sync_insert_new_post', $post_id, $post_args );
 
-		$data['id'] = $post_id;
-
-		return wp_send_json_success( $data );
+		return wp_send_json_success( array( 'debug' => array(
+			'remote_post_id'	=> $post_args['meta_input']['press_sync_post_id'],
+			'local_post_id'		=> $local_post_id,
+			'message'			=> __( 'The post has been synced with the remote server', 'press-sync' ),
+		) ) );
 
 	}
 
@@ -292,25 +290,24 @@ class Press_Sync_API {
 
 	}
 
-	public function post_exists( $post_args ) {
+	public function get_synced_post( $post_args ) {
 
+		global $wpdb;
+
+		// Capture the press sync post ID
 		$press_sync_post_id = isset( $post_args['meta_input']['press_sync_post_id'] ) ? $post_args['meta_input']['press_sync_post_id'] : 0;
 
-		$query_args = array(
-			'post_type' 		=> $post_args['post_type'],
-			'posts_per_page' 	=> 1,
-			'meta_key'			=> 'press_sync_post_id',
-			'meta_value'		=> $press_sync_post_id,
-			'post_status'		=> 'any',
-		);
+		$sql = "
+			SELECT post_id AS ID, post_type, post_modified FROM $wpdb->postmeta AS meta
+			LEFT JOIN $wpdb->posts AS posts ON posts.ID = meta.post_id
+			WHERE meta.meta_key = 'press_sync_post_id' AND meta.meta_value = %d AND posts.post_type = %s
+			LIMIT 1
+		";
 
-		$post = get_posts( $query_args );
+		$prepared_sql = $wpdb->prepare( $sql, $press_sync_post_id, $post_args['post_type'] );
+		$post = $wpdb->get_row( $prepared_sql, ARRAY_A );
 
-		if ( $post ) {
-			return (array) $post[0];
-		}
-
-		return false;
+		return ( $post ) ? $post : false;
 
 	}
 
@@ -374,29 +371,33 @@ class Press_Sync_API {
 
 	}
 
+	/**
+	 * Returns the original author ID of the synced post
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param integer $user_id
+	 * @return integer $user_id
+	 */
 	public function get_press_sync_author_id( $user_id ) {
 
 		if ( ! $user_id ) {
 			return 0;
 		}
 
-		$args = array(
-			'fields'		=> array('ID'),
-			'meta_key'		=> 'press_sync_user_id',
-			'meta_value'	=> $user_id
-		);
+		global $wpdb;
 
-		$user = get_users( $args );
+		$sql = "SELECT user_id AS ID FROM $wpdb->usermeta WHERE meta_key = 'press_sync_user_id' AND meta_value = %d";
+		$prepared_sql = $wpdb->prepare( $sql, $user_id );
 
-		if ( $user ) {
-			return $user[0]->ID;
-		}
+		$press_sync_user_id = $wpdb->get_var( $prepared_sql );
 
-		return $user_id;
+		return ( $press_sync_user_id ) ? $press_sync_user_id : 0;
 
 	}
 
 	public function attach_featured_image( $post_id, $post_args ) {
+
 		// Post does not have a featured image so bail early.
 		if ( empty( $post_args['featured_image'] ) ) {
 			return false;
@@ -513,6 +514,50 @@ class Press_Sync_API {
 				wp_insert_comment( $comment );
 			}
 		}
+	}
+
+	/**
+	 * Checks to see if a non-synced duplicate exists
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $post_name
+	 * @param string $post_type
+	 * @return WP_Post
+	 */
+	public function get_non_synced_duplicate( $post_name, $post_type ) {
+
+		global $wpdb;
+
+		$sql = "SELECT ID, post_type, post_modified FROM $wpdb->posts WHERE post_name = %s AND post_type = %s";
+		$prepared_sql = $wpdb->prepare( $sql, $post_name, $post_type );
+
+		$post = $wpdb->get_row( $prepared_sql, ARRAY_A );
+
+		return ( $post ) ? $post : false;
+
+	}
+
+	/**
+	 * Assign a WP Object the missing press sync ID
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array $object_args
+	 * @return boolean
+	 */
+	public function add_press_sync_id( $object_id, $object_args ) {
+
+		if ( ! isset( $object_args['post_type'] ) ) {
+			return false;
+		}
+
+		$press_sync_post_id = isset( $object_args['meta_input']['press_sync_post_id'] ) ? $object_args['meta_input']['press_sync_post_id'] : '';
+		$press_sync_source = isset( $object_args['meta_input']['press_sync_source'] ) ? $object_args['meta_input']['press_sync_source'] : '';
+
+		update_post_meta( $object_id, 'press_sync_post_id', $press_sync_post_id );
+		update_post_meta( $object_id, 'press_sync_source', $press_sync_source );
+
 	}
 
 }
