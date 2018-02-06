7846d5bfdd66a84ed012e4b4f14ec21c92e35afe
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-02-06 16:39:21 -0600

    Actually add post route to API.

--- includes/validation/Post.php
+++ includes/validation/Post.php
@@ -1,7 +1,15 @@
 <?php
 namespace Press_Sync\validation;
 
+/**
+ * Class Post
+ *
+ * @package Press_Sync\validation
+ */
 class Post implements CountInterface {
+	/**
+	 *
+	 */
 	public function get_count() {
 		// TODO: Implement get_count() method.
 	}
